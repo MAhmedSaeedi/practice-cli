1e4be7858bbb2cf3a26bca1aee04f68196c86081
MAhmedSaeedi <m.ahmed.saeedi@outlook.com>
2023-08-17 20:08:13 +0100

    cli init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-const hello = () => {}
+#!/usr/bin/env node
+
+import { Command } from "commander"
+
+const program = new Command()
+
+program
+.argument("<string>","string to log")
+
+.action((message:string)=>{
+    console.log(`Hello ${message}`)
+})
+.description("Say hello")
+
+program.parse(process.argv)
